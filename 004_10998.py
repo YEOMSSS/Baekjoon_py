48423ed985f2007d120e326c47719da95a7159c1
YEOM <junhayeom@gmail.com>
2025-04-16 21:22:43 +0900

    250416

---
+++ 004_10998.py
@@ -0,0 +1,29 @@
+'''
+문제
+두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+출력
+첫째 줄에 A×B를 출력한다.
+
+예제 입력 1 
+1 2
+예제 출력 1 
+2
+예제 입력 2 
+3 4
+예제 출력 2 
+12
+'''
+
+first_int, second_int = input().split()
+
+first_int = int(first_int)
+second_int = int(second_int)
+
+if 0 < first_int < 10 and 0 < second_int < 10:
+    print(first_int * second_int)
+else:
+    print("올바른 수를 입력하세요.")
