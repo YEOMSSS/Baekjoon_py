ae137cb760dfdd0aa89622cf56a78dfb7648e992
YEOM <junhayeom@gmail.com>
2025-05-27 09:06:30 +0900

    250527

--- memo.py
+++ memo.py
@@ -1,6 +1,19 @@
-def gcd_maker(x, y):
-    while y:
-        x, y = y, x % y
-    return x
+num = int(input())
 
-print(gcd_maker(20, 10))
+pack_prices = list(map(int, input().split()))
+
+answer = 0
+while num != 0:
+    card_prices = []
+
+    index_max = 0
+    for i in range(num):
+        if pack_prices[i] * (index_max + 1) > pack_prices[index_max] * (i + 1):
+                    index_max = i
+
+    answer += pack_prices[index_max] * (num // (index_max + 1))
+    # 가장 비싼 팩을 최대한 많이 구매한다.
+    
+    num %= index_max + 1
+
+print(answer)
